98ccb1f46c090c6d23927f1a3e7fb55662998529
Michael <michaelnicholasfisher@gmail.com>
2013-11-29 14:37:36 +0000

    Creates and tests plane

---
+++ lib/plane.rb
@@ -0,0 +1,24 @@
+class Plane
+
+def initialize
+	flying
+end
+
+def flying?
+	@flying
+end
+
+def land
+	 @flying = false
+end
+
+
+
+def flying
+	@flying = true
+end
+
+
+
+
+end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,1 +1,22 @@
-plane_spec.rb
+require_relative'../lib/plane'
+
+describe Plane do 
+
+	let(:plane) {Plane.new}
+
+	it 'should be in the air when created' do
+		expect(plane).to be_flying
+	end
+
+	it 'should be able to land' do
+		plane.land
+		expect(plane.flying?).to be_false
+	end
+
+	it 'should be able to take off again after landing' do
+		plane.land
+		plane.flying
+		expect(plane.flying?).to be_true
+	end
+
+end
